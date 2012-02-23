36d77168a3e6e6b0256a8af3638281d01227d0e9
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-02-23 15:58:36 +0100

    fc.sh working properly

--- src/fc.sh
+++ src/fc.sh
@@ -27,8 +27,11 @@ CONFIG_DIR=~/.fastcuda
 
 SYSTEM=lin
 if [ `uname -m | grep x86_64` ]; then
-	SYSTEM=${XTCLSH_DIR}64  # lin64 for 64 bits systems
+	SYSTEM=${SYSTEM}64  # lin64 for 64 bits systems
 fi
+
+XILINX_DIR_TOKEN=/ISE/bin/$SYSTEM/xtclsh
+FASTCUDA_DIR_TOKEN=/src/lib/run_task.tcl
 
 
 # Check if the configuration file exists. If it doesn't exist, create
@@ -38,58 +41,75 @@ if [ ! -d $CONFIG_DIR ]; then
 	echo "No configuration folder found, creating a new one..."
 
 	mkdir $CONFIG_DIR
-	touch ${CONFIG_DIR}/config
+	touch $CONFIG_DIR/config
 
-	# ls -R is faster than find for this purpose, and automatically
-	# excludes hidden folders.
 	echo "Trying to find Xilinx in your /opt and user directories..."
-	# TODO: testing paths! replace them with the correspondins /opt and ~.
-	XILINX_FOUND_OPT=$(ls -R /media/Data/Programs | grep ISE/bin/lin)
-	XILINX_FOUND_HOME=$(ls -R /media/Data/Development | grep ISE/bin/lin)
+	XILINX_FOUND_OPT=$(find /opt -name '.?*' -prune -o -print | grep $XILINX_DIR_TOKEN)
+	XILINX_FOUND_HOME=$(find ~ -name '.?*' -prune -o -print | grep $XILINX_DIR_TOKEN)
+
 
 	if [[ -z "$XILINX_FOUND_OPT" && -z "$XILINX_FOUND_HOME" ]]; then
 		echo "Couldn't find Xilinx in your system."
 		echo "Please, introduce path for the Xilinx installation directory,"
 		read -p "(i.e.: /opt/Xilinx/13.3/ISE_DS): "
-		while [ ! `readlink -e ${REPLY}/ISE` ]; do
+		while [ ! `readlink -e ${REPLY}${XILINX_DIR_TOKEN}` ]; do
 			echo "Incorrect path. Please try again with an absolute path with no trailing '/',"
 			read -p "(i.e.: /opt/Xilinx/13.3/ISE_DS): "
 		done
-		echo XILINX_DIR=$REPLY>> ${CONFIG_DIR}/config
+		echo XILINX_DIR=$REPLY>> $CONFIG_DIR/config
 	else
 		if [[ ! -z "$XILINX_FOUND_OPT" ]]; then
 			echo "Found Xilinx installation in /opt."
-			# TODO: write the corresponding path in the config file
+			# Make use of parameter expansion:
+			echo XILINX_DIR=${XILINX_FOUND_OPT%%"$XILINX_DIR_TOKEN"*}>> $CONFIG_DIR/config
 		else
 			echo "Found Xilinx installation in your user directory."
-			# TODO: write the corresponding path in the config file
+			# Make use of parameter expansion:
+			echo XILINX_DIR=${XILINX_FOUND_HOME%%"$XILINX_DIR_TOKEN"*}>> $CONFIG_DIR/config
 		fi
 	fi
 
-	# TODO: use same procedure as for Xilinx installation path (see above).
-	echo "Please, introduce path for the FASTCUDA installation directory (i.e.: /opt/fastcuda):"
-	read
-	while [ ! `readlink -e ${REPLY}/src` ]; do
-		echo "Incorrect path. Please try again with an absolute path with no trailing '/' (i.e.: /opt/fastcuda):"
-		read
-	done
-	echo FC_DIR=$REPLY>> ${CONFIG_DIR}/config
+
+	echo "Trying to find FASTCUDA in your /opt and user directories..."
+	FASTCUDA_FOUND_OPT=$(find /opt -name '.?*' -prune -o -print | grep $FASTCUDA_DIR_TOKEN)
+	FASTCUDA_FOUND_HOME=$(find ~ -name '.?*' -prune -o -print | grep $FASTCUDA_DIR_TOKEN)
+
+	if [[ -z "$FASTCUDA_FOUND_OPT" && -z "$FASTCUDA_FOUND_HOME" ]]; then
+		echo "Couldn't find FASTCUDA in your system."
+		echo "Please, introduce path for the FASTCUDA installation directory,"
+		read -p "(i.e.: /opt/fastcuda): "
+		while [ ! `readlink -e ${REPLY}${FASTCUDA_DIR_TOKEN}` ]; do
+			echo "Incorrect path. Please try again with an absolute path with no trailing '/',"
+			read -p "(i.e.: /opt/fastcuda): "
+		done
+		echo FASTCUDA_DIR=$REPLY>> $CONFIG_DIR/config
+	else
+		if [[ ! -z "$FASTCUDA_FOUND_OPT" ]]; then
+			echo "Found FASTCUDA installation in /opt."
+			# Make use of parameter expansion:
+			echo FASTCUDA_DIR=${FASTCUDA_FOUND_OPT%%"$FASTCUDA_DIR_TOKEN"*}>> $CONFIG_DIR/config
+		else
+			echo "Found FASTCUDA installation in your user directory."
+			# Make use of parameter expansion:
+			echo FASTCUDA_DIR=${FASTCUDA_FOUND_HOME%%"$FASTCUDA_DIR_TOKEN"*}>> $CONFIG_DIR/config
+		fi
+	fi
+
+	echo "Configuration file created."
 fi
 
 
 # Get information from the configuration file:
 echo "Reading configuration file..."
-source ${CONFIG_DIR}/config
+source $CONFIG_DIR/config
+echo "Loaded data from the configuration file."
 
 
 # Find Xilinx's TCL native interpreter (depends on the system's
 # architecture):
-XTCLSH_DIR=${XILINX_DIR}/ISE/bin/lin
-if [ `uname -m | grep x86_64` ]; then
-	XTCLSH_DIR=${XTCLSH_DIR}64  # lin64 for 64 bits systems
-fi
+XTCLSH_DIR=$XILINX_DIR/ISE/bin/$SYSTEM
 
 
 # Run the target TCL file using Xilinx's tclsh
-${XTCLSH_DIR}/xtclsh ${FC_DIR}/src/main.tcl $1
+$XTCLSH_DIR/xtclsh $FASTCUDA_DIR/src/main.tcl $1
 
