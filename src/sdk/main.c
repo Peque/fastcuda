5b7651f5460ce86b3f36fd0896571c0722499679
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-09-06 18:17:05 +0200

    Shared memory accessible from the MB through the registers

--- src/sdk/main.c
+++ src/sdk/main.c
@@ -44,18 +44,31 @@ typedef volatile int32_t * pdata_32;
 #define addr_3       REG_DATA_32(3)
 #define go_flag      REG_DATA_32(4)
 #define ready_flag   REG_DATA_32(5)
-#define DOA          REG_DATA_32(6)
-#define DOB          REG_DATA_32(7)
-#define ADDRA        REG_DATA_32(8)
-#define ADDRB        REG_DATA_32(9)
-#define ENA          REG_DATA_32(10)
-#define ENB          REG_DATA_32(11)
-#define RSTA         REG_DATA_32(12)
-#define RSTB         REG_DATA_32(13)
-#define DIA          REG_DATA_32(14)
-#define DIB          REG_DATA_32(15)
-#define WEA          REG_DATA_32(16)
-#define WEB          REG_DATA_32(17)
+#define DO_0         REG_DATA_32(6)
+#define DO_1         REG_DATA_32(7)
+#define DO_2         REG_DATA_32(8)
+#define DO_3         REG_DATA_32(9)
+#define DI_0         REG_DATA_32(10)
+#define DI_1         REG_DATA_32(11)
+#define DI_2         REG_DATA_32(12)
+#define DI_3         REG_DATA_32(13)
+#define ADDR_0       REG_DATA_32(14)
+#define ADDR_1       REG_DATA_32(15)
+#define ADDR_2       REG_DATA_32(16)
+#define ADDR_3       REG_DATA_32(17)
+#define RST          REG_DATA_32(18)
+#define WE_0         REG_DATA_32(19)
+#define WE_1         REG_DATA_32(20)
+#define WE_2         REG_DATA_32(21)
+#define WE_3         REG_DATA_32(22)
+#define REQ_0        REG_DATA_32(23)
+#define REQ_1        REG_DATA_32(24)
+#define REQ_2        REG_DATA_32(25)
+#define REQ_3        REG_DATA_32(26)
+#define RDY_0        REG_DATA_32(27)
+#define RDY_1        REG_DATA_32(28)
+#define RDY_2        REG_DATA_32(29)
+#define RDY_3        REG_DATA_32(30)
 
 // We need this header to avoid compiling errors...
 void xil_printf(const char *, ...);
@@ -103,23 +116,50 @@ int main(void)
 	//for (i = 0; i < 16; i++) xil_printf("%X => %d\n", &DDR2_DATA_32(i), DDR2_DATA_32(i));
 	//for (i = 0; i < 16; i++) xil_printf("%X => %d\n", &DDR2_DATA_32(i+100), DDR2_DATA_32(i+100));
 
-	ENA = ENB = 1;
-	RSTA = RSTB = 0;
+	DI_0 = 0xAAAAAAAA;
+	DI_1 = 0xBBBBBBBB;
+	DI_2 = 0xCCCCCCCC;
+	DI_3 = 0xDDDDDDDD;
 
-	DIA = 0xAAAAAAAA;
-	DIB = 0xBBBBBBBB;
+	ADDR_0 = 100;
+	ADDR_1 = 200;
+	ADDR_2 = 600;
+	ADDR_3 = 700;
 
-	ADDRA = 100;
-	ADDRB = 200;
+	WE_0 = 0b1111;
+	WE_1 = 0b1111;
+	WE_2 = 0b1111;
+	WE_3 = 0b1111;
 
-	WEA = 0b1111;
-	WEB = 0b1111;
+	REQ_0 = 1;
+	while (!RDY_0);
+	REQ_0 = 0;
+	REQ_1 = 1;
+	while (!RDY_1);
+	REQ_1 = 0;
+	REQ_2 = 1;
+	while (!RDY_2);
+	REQ_2 = 0;
+	REQ_3 = 1;
+	while (!RDY_3);
+	REQ_3 = 0;
 
-	ADDRA = 200;
-	ADDRB = 100;
+	ADDR_0 = 700;
+	ADDR_1 = 600;
+	ADDR_2 = 200;
+	ADDR_3 = 100;
 
-	xil_printf("Data read in port A => %x\n", DOA);
-	xil_printf("Data read in port B => %x\n", DOB);
+	REQ_0 = 1;
+	REQ_1 = 1;
+	REQ_2 = 1;
+	REQ_3 = 1;
+
+	while (!RDY_3);
+
+	xil_printf("Data read in port 0 => %x\n", DO_0);
+	xil_printf("Data read in port 1 => %x\n", DO_1);
+	xil_printf("Data read in port 2 => %x\n", DO_2);
+	xil_printf("Data read in port 3 => %x\n", DO_3);
 
 	return 0;
 }
