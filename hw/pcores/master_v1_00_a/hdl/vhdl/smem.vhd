e6780d50e6c54bf85d62bc55fb79b6f0fb28ec01
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-09-18 20:24:59 +0200

    Optimized kX_output_sel(0) signals

--- hw/pcores/master_v1_00_a/hdl/vhdl/smem.vhd
+++ hw/pcores/master_v1_00_a/hdl/vhdl/smem.vhd
@@ -171,89 +171,21 @@ begin
 
 	--
 	-- The lower bit of the output selection signal represent the BRAM
-	-- port that may be being used:
+	-- port that may be being used. For each BRAM (kX_output_sel(1)), we
+	-- see if the kernel is connected to port B (if not, then we suppose
+	-- port A:
 	--
-	--   kX_output_sel(0) = (ADDR_0(8 downto 0) == addr_0_b) or (ADDR_0(8 downto 0) == addr_1_b)
-	--
-	k0_output_sel(0) <= ( ( to_bit(ADDR_0(0)) xnor to_bit(addr_0_b(0)) ) and
-	                      ( to_bit(ADDR_0(1)) xnor to_bit(addr_0_b(1)) ) and
-	                      ( to_bit(ADDR_0(2)) xnor to_bit(addr_0_b(2)) ) and
-	                      ( to_bit(ADDR_0(3)) xnor to_bit(addr_0_b(3)) ) and
-	                      ( to_bit(ADDR_0(4)) xnor to_bit(addr_0_b(4)) ) and
-	                      ( to_bit(ADDR_0(5)) xnor to_bit(addr_0_b(5)) ) and
-	                      ( to_bit(ADDR_0(6)) xnor to_bit(addr_0_b(6)) ) and
-	                      ( to_bit(ADDR_0(7)) xnor to_bit(addr_0_b(7)) ) and
-	                      ( to_bit(ADDR_0(8)) xnor to_bit(addr_0_b(8)) ) )
-	                    or
-	                    ( ( to_bit(ADDR_0(0)) xnor to_bit(addr_1_b(0)) ) and
-	                      ( to_bit(ADDR_0(1)) xnor to_bit(addr_1_b(1)) ) and
-	                      ( to_bit(ADDR_0(2)) xnor to_bit(addr_1_b(2)) ) and
-	                      ( to_bit(ADDR_0(3)) xnor to_bit(addr_1_b(3)) ) and
-	                      ( to_bit(ADDR_0(4)) xnor to_bit(addr_1_b(4)) ) and
-	                      ( to_bit(ADDR_0(5)) xnor to_bit(addr_1_b(5)) ) and
-	                      ( to_bit(ADDR_0(6)) xnor to_bit(addr_1_b(6)) ) and
-	                      ( to_bit(ADDR_0(7)) xnor to_bit(addr_1_b(7)) ) and
-	                      ( to_bit(ADDR_0(8)) xnor to_bit(addr_1_b(8)) ) );
+	k0_output_sel(0) <=  ( not k0_output_sel(1) and not bram_0_B_input_sel(1) and not bram_0_B_input_sel(0) ) or
+	                     (     k0_output_sel(1) and not bram_1_B_input_sel(1) and not bram_1_B_input_sel(0) );
 
-	k1_output_sel(0) <= ( ( to_bit(ADDR_1(0)) xnor to_bit(addr_0_b(0)) ) and
-	                      ( to_bit(ADDR_1(1)) xnor to_bit(addr_0_b(1)) ) and
-	                      ( to_bit(ADDR_1(2)) xnor to_bit(addr_0_b(2)) ) and
-	                      ( to_bit(ADDR_1(3)) xnor to_bit(addr_0_b(3)) ) and
-	                      ( to_bit(ADDR_1(4)) xnor to_bit(addr_0_b(4)) ) and
-	                      ( to_bit(ADDR_1(5)) xnor to_bit(addr_0_b(5)) ) and
-	                      ( to_bit(ADDR_1(6)) xnor to_bit(addr_0_b(6)) ) and
-	                      ( to_bit(ADDR_1(7)) xnor to_bit(addr_0_b(7)) ) and
-	                      ( to_bit(ADDR_1(8)) xnor to_bit(addr_0_b(8)) ) )
-	                    or
-	                    ( ( to_bit(ADDR_1(0)) xnor to_bit(addr_1_b(0)) ) and
-	                      ( to_bit(ADDR_1(1)) xnor to_bit(addr_1_b(1)) ) and
-	                      ( to_bit(ADDR_1(2)) xnor to_bit(addr_1_b(2)) ) and
-	                      ( to_bit(ADDR_1(3)) xnor to_bit(addr_1_b(3)) ) and
-	                      ( to_bit(ADDR_1(4)) xnor to_bit(addr_1_b(4)) ) and
-	                      ( to_bit(ADDR_1(5)) xnor to_bit(addr_1_b(5)) ) and
-	                      ( to_bit(ADDR_1(6)) xnor to_bit(addr_1_b(6)) ) and
-	                      ( to_bit(ADDR_1(7)) xnor to_bit(addr_1_b(7)) ) and
-	                      ( to_bit(ADDR_1(8)) xnor to_bit(addr_1_b(8)) ) );
-
-	k2_output_sel(0) <= ( ( to_bit(ADDR_2(0)) xnor to_bit(addr_0_b(0)) ) and
-	                      ( to_bit(ADDR_2(1)) xnor to_bit(addr_0_b(1)) ) and
-	                      ( to_bit(ADDR_2(2)) xnor to_bit(addr_0_b(2)) ) and
-	                      ( to_bit(ADDR_2(3)) xnor to_bit(addr_0_b(3)) ) and
-	                      ( to_bit(ADDR_2(4)) xnor to_bit(addr_0_b(4)) ) and
-	                      ( to_bit(ADDR_2(5)) xnor to_bit(addr_0_b(5)) ) and
-	                      ( to_bit(ADDR_2(6)) xnor to_bit(addr_0_b(6)) ) and
-	                      ( to_bit(ADDR_2(7)) xnor to_bit(addr_0_b(7)) ) and
-	                      ( to_bit(ADDR_2(8)) xnor to_bit(addr_0_b(8)) ) )
-	                    or
-	                    ( ( to_bit(ADDR_2(0)) xnor to_bit(addr_1_b(0)) ) and
-	                      ( to_bit(ADDR_2(1)) xnor to_bit(addr_1_b(1)) ) and
-	                      ( to_bit(ADDR_2(2)) xnor to_bit(addr_1_b(2)) ) and
-	                      ( to_bit(ADDR_2(3)) xnor to_bit(addr_1_b(3)) ) and
-	                      ( to_bit(ADDR_2(4)) xnor to_bit(addr_1_b(4)) ) and
-	                      ( to_bit(ADDR_2(5)) xnor to_bit(addr_1_b(5)) ) and
-	                      ( to_bit(ADDR_2(6)) xnor to_bit(addr_1_b(6)) ) and
-	                      ( to_bit(ADDR_2(7)) xnor to_bit(addr_1_b(7)) ) and
-	                      ( to_bit(ADDR_2(8)) xnor to_bit(addr_1_b(8)) ) );
-
-	k3_output_sel(0) <= ( ( to_bit(ADDR_3(0)) xnor to_bit(addr_0_b(0)) ) and
-	                      ( to_bit(ADDR_3(1)) xnor to_bit(addr_0_b(1)) ) and
-	                      ( to_bit(ADDR_3(2)) xnor to_bit(addr_0_b(2)) ) and
-	                      ( to_bit(ADDR_3(3)) xnor to_bit(addr_0_b(3)) ) and
-	                      ( to_bit(ADDR_3(4)) xnor to_bit(addr_0_b(4)) ) and
-	                      ( to_bit(ADDR_3(5)) xnor to_bit(addr_0_b(5)) ) and
-	                      ( to_bit(ADDR_3(6)) xnor to_bit(addr_0_b(6)) ) and
-	                      ( to_bit(ADDR_3(7)) xnor to_bit(addr_0_b(7)) ) and
-	                      ( to_bit(ADDR_3(8)) xnor to_bit(addr_0_b(8)) ) )
-	                    or
-	                    ( ( to_bit(ADDR_3(0)) xnor to_bit(addr_1_b(0)) ) and
-	                      ( to_bit(ADDR_3(1)) xnor to_bit(addr_1_b(1)) ) and
-	                      ( to_bit(ADDR_3(2)) xnor to_bit(addr_1_b(2)) ) and
-	                      ( to_bit(ADDR_3(3)) xnor to_bit(addr_1_b(3)) ) and
-	                      ( to_bit(ADDR_3(4)) xnor to_bit(addr_1_b(4)) ) and
-	                      ( to_bit(ADDR_3(5)) xnor to_bit(addr_1_b(5)) ) and
-	                      ( to_bit(ADDR_3(6)) xnor to_bit(addr_1_b(6)) ) and
-	                      ( to_bit(ADDR_3(7)) xnor to_bit(addr_1_b(7)) ) and
-	                      ( to_bit(ADDR_3(8)) xnor to_bit(addr_1_b(8)) ) );
+	k1_output_sel(0) <=  ( not k1_output_sel(1) and not bram_0_B_input_sel(1) and     bram_0_B_input_sel(0) ) or
+	                     (     k1_output_sel(1) and not bram_1_B_input_sel(1) and     bram_1_B_input_sel(0) );
+	                     
+	k2_output_sel(0) <=  ( not k2_output_sel(1) and     bram_0_B_input_sel(1) and not bram_0_B_input_sel(0) ) or
+	                     (     k2_output_sel(1) and     bram_1_B_input_sel(1) and not bram_1_B_input_sel(0) );
+	                     
+	k3_output_sel(0) <=  ( not k3_output_sel(1) and     bram_0_B_input_sel(1) and     bram_0_B_input_sel(0) ) or
+	                     (     k3_output_sel(1) and     bram_1_B_input_sel(1) and     bram_1_B_input_sel(0) );
 
 
 	input_controller_0 : block begin
