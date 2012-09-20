31f3881f3c764d9455e7a2a119ad6f434ac1fa95
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-09-20 23:12:40 +0200

    Fixed broadcast when various write requests are found

--- hw/pcores/master_v1_00_a/hdl/vhdl/smem.vhd
+++ hw/pcores/master_v1_00_a/hdl/vhdl/smem.vhd
@@ -155,25 +155,25 @@ begin
 	en_1_b <= '1';
 
 
-	we_0_safe(3) <= WE_0(3) and REQ_0;
-	we_0_safe(2) <= WE_0(2) and REQ_0;
-	we_0_safe(1) <= WE_0(1) and REQ_0;
-	we_0_safe(0) <= WE_0(0) and REQ_0;
+	we_0_safe(3) <= WE_0(3) and to_stdulogic(k0_needs_attention);
+	we_0_safe(2) <= WE_0(2) and to_stdulogic(k0_needs_attention);
+	we_0_safe(1) <= WE_0(1) and to_stdulogic(k0_needs_attention);
+	we_0_safe(0) <= WE_0(0) and to_stdulogic(k0_needs_attention);
 
-	we_1_safe(3) <= WE_1(3) and REQ_1;
-	we_1_safe(2) <= WE_1(2) and REQ_1;
-	we_1_safe(1) <= WE_1(1) and REQ_1;
-	we_1_safe(0) <= WE_1(0) and REQ_1;
+	we_1_safe(3) <= WE_1(3) and to_stdulogic(k1_needs_attention);
+	we_1_safe(2) <= WE_1(2) and to_stdulogic(k1_needs_attention);
+	we_1_safe(1) <= WE_1(1) and to_stdulogic(k1_needs_attention);
+	we_1_safe(0) <= WE_1(0) and to_stdulogic(k1_needs_attention);
 
-	we_2_safe(3) <= WE_2(3) and REQ_2;
-	we_2_safe(2) <= WE_2(2) and REQ_2;
-	we_2_safe(1) <= WE_2(1) and REQ_2;
-	we_2_safe(0) <= WE_2(0) and REQ_2;
+	we_2_safe(3) <= WE_2(3) and to_stdulogic(k2_needs_attention);
+	we_2_safe(2) <= WE_2(2) and to_stdulogic(k2_needs_attention);
+	we_2_safe(1) <= WE_2(1) and to_stdulogic(k2_needs_attention);
+	we_2_safe(0) <= WE_2(0) and to_stdulogic(k2_needs_attention);
 
-	we_3_safe(3) <= WE_3(3) and REQ_3;
-	we_3_safe(2) <= WE_3(2) and REQ_3;
-	we_3_safe(1) <= WE_3(1) and REQ_3;
-	we_3_safe(0) <= WE_3(0) and REQ_3;
+	we_3_safe(3) <= WE_3(3) and to_stdulogic(k3_needs_attention);
+	we_3_safe(2) <= WE_3(2) and to_stdulogic(k3_needs_attention);
+	we_3_safe(1) <= WE_3(1) and to_stdulogic(k3_needs_attention);
+	we_3_safe(0) <= WE_3(0) and to_stdulogic(k3_needs_attention);
 
 
 	RDY_0 <= to_stdulogic(k0_being_served);
